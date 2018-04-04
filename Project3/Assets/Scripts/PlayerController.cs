037bbb87cb0a1151b1409974518bc4a9c41e3a90
Leon <gozirra21@gmail.com>
2018-04-03 20:31:20 -0500

    Comments, Boundary and Camera changes
    Test

--- Project3/Assets/Scripts/PlayerController.cs
+++ Project3/Assets/Scripts/PlayerController.cs
@@ -2,47 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+//allows the variables to be serialized and collapsible
 [System.Serializable]
-public class Boundary
+public class MapBounds
 {
-	public float xMin, xMax, zMin, zMax;
+    public float xMin, xMax, zMin, zMax;
 }
 
 public class PlayerController : MonoBehaviour {
-
-	private Rigidbody rb;
-	public float speed;
-	public float tilt;
-	public Boundary boundary;
+    //variable declaration of a rigidbody ship component, the playable field, laser object, and laser spawn transform component which follows the ship
+    private Rigidbody shipComp;
+    public MapBounds map;
     public GameObject Laser;
     public Transform startLaser;
+    //variable declaration of the speed of the ship, the tilt when moving, the rate that it can fire, and the shot time checker
+    public float speed;
+    public float tilt;
     public float fireRate;
-    private float nextFire;
+    private float nextShot;
 
-	void Start(){
-		rb = GetComponent<Rigidbody> ();
-	}
+    void Start(){
+        //assigns rigidbody component to ship variable
+        shipComp = GetComponent<Rigidbody>();
+    }
 
     void Update() {
-        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) && Time.time > nextFire)
+        //checks if either the keyboard or right mouse button is clicked, and if enough time has passed for another shot
+        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) && Time.time > nextShot)
         {
-            nextFire = Time.time + fireRate;
+            //iterates the next time a shot is available by the ship's rate of fire
+            nextShot = Time.time + fireRate;
+            //instantiates the laser object using the spawn location's position and rotation
             Instantiate(Laser, startLaser.position, startLaser.rotation);
         }
     }
 
     void FixedUpdate(){
-		//float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3 (-moveVertical, 0.0f, 0.0f);
-		rb.velocity = movement * speed;
-
-		rb.position = new Vector3 (
-			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
-		);
-		rb.rotation = Quaternion.Euler (0.0f, 0.0f, (rb.velocity.x * -tilt));
-	}
+        //declaration of movement variable
+        float moveVertical = Input.GetAxis("Vertical");
+        //only allows movement vertically
+        Vector3 movement = new Vector3(-moveVertical, 0.0f, 0.0f);
+        //sets the velocity via speed variable
+        shipComp.velocity = movement * speed;
+        //keeps the ship from going out of bounds
+        shipComp.position = new Vector3(Mathf.Clamp(shipComp.position.x, map.xMin, map.xMax), 0.0f, Mathf.Clamp(shipComp.position.z, map.zMin, map.zMax));
+        //tilts the ship when moving
+        shipComp.rotation = Quaternion.Euler(0.0f, 0.0f, (shipComp.velocity.x * -tilt));
+    }
 }
--- Project3/Assets/Scripts/laserMovement.cs
+++ Project3/Assets/Scripts/laserMovement.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class laserMovement : MonoBehaviour {
-    private Rigidbody comp; //creates rigidbody
-    public float speed; //speed variable
+    //laser component variable
+    private Rigidbody laserComp;
+    //variable for the speed of the laser
+    public float speed; 
 	void Start () {
-        comp = GetComponent<Rigidbody>();          
-        comp.velocity = transform.forward * speed;  //moves object along the Z-axis at a set speed
+        //assigns rigidbody component to laser variable
+        laserComp = GetComponent<Rigidbody>();
+        //moves object along the Z-axis at a set speed
+        laserComp.velocity = transform.forward * speed;  
 	}
 }
