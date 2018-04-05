aaeecfaeccb4db37979dad6df9bfa69aa00c3c4b
David Iordan <davidiordan@icloud.com>
2018-04-05 12:47:00 -0500

    Added and linked space ship model

--- Project3/Assets/Scripts/PlayerController.cs
+++ Project3/Assets/Scripts/PlayerController.cs
@@ -45,8 +45,8 @@ public class PlayerController : MonoBehaviour {
         //sets the velocity via speed variable
         shipComp.velocity = movement * speed;
         //keeps the ship from going out of bounds
-        shipComp.position = new Vector3(Mathf.Clamp(shipComp.position.x, map.xMin, map.xMax), 0.0f, Mathf.Clamp(shipComp.position.z, map.zMin, map.zMax));
+        shipComp.position = new Vector3(Mathf.Clamp(shipComp.position.x, map.xMin, map.xMax), 0.0f, -3);
         //tilts the ship when moving
-        shipComp.rotation = Quaternion.Euler(0.0f, 0.0f, (shipComp.velocity.x * -tilt));
+		shipComp.rotation = Quaternion.Euler((shipComp.velocity.x * -tilt), -90, -78);
     }
 }
